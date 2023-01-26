7d9f5d05760924462f0909ff0c48296e9cf8d856
lostmart <lostmart@hotmail.com>
2023-01-26 10:11:51 +0100

    new filters

--- js/factories/CardFn.js
+++ js/factories/CardFn.js
@@ -1,5 +1,5 @@
-import RecipeModel from "../classes/RecipeModel.js"
-import { filterDom } from "../script.js"
+import RecipeModel from '../classes/RecipeModel.js'
+import { filterDom, filterDomByTags } from '../script.js'
 
 // ingredients from all the recipies
 export let ingredientsArray = []
@@ -7,27 +7,27 @@ export let ustensilsArray = []
 export let appliance = []
 
 // arrays for filters
-let ingredientsArrayFilter = []
+export let ingredientsArrayFilter = []
 
 //  returns a DOM element ready to append
 function domCard(card) {
 	// creates an object based on the CardModel class
 	const recipe = new RecipeModel(card)
 	// template from index.html
-	const card_template = document.querySelector("[data-card-template]")
+	const card_template = document.querySelector('[data-card-template]')
 
 	// elements for the card
 	const domCard = card_template.content.cloneNode(true).children[0]
-	const cardImg = domCard.querySelector("[data-card-img]")
-	const cardTime = domCard.querySelector("[data-card-time]")
+	const cardImg = domCard.querySelector('[data-card-img]')
+	const cardTime = domCard.querySelector('[data-card-time]')
 	cardTime.children[1].textContent = `${recipe.time} min`
-	const cardTitle = domCard.querySelector(".card-title")
-	const cardText = domCard.querySelector(".card-text")
-	const ul = domCard.querySelector(".list-group")
+	const cardTitle = domCard.querySelector('.card-title')
+	const cardText = domCard.querySelector('.card-text')
+	const ul = domCard.querySelector('.list-group')
 	// ingredients
 	recipe.ingredients.forEach((ingr) => {
-		const li = document.createElement("li")
-		li.classList.add("list-group-item")
+		const li = document.createElement('li')
+		li.classList.add('list-group-item')
 		li.innerHTML = `<strong>${ingr.ingredient}:</strong> ${ingr.quantity} `
 		// checks if there is units of measure
 		ingr.unit ? (li.innerHTML += ingr.unit) : false
@@ -44,9 +44,9 @@ function domCard(card) {
 	//console.log(ingredientsArray)
 	//console.log(ustensilsArray)
 
-	cardImg.src = "../assets/bg-img.jpg"
+	cardImg.src = '../assets/bg-img.jpg'
 	cardImg.alt = recipe.name
-	cardText.textContent = recipe.description.slice(0, 174) + "..."
+	cardText.textContent = recipe.description.slice(0, 174) + '...'
 	cardTitle.textContent = recipe.name
 
 	return domCard
@@ -59,16 +59,16 @@ function handleListClick(value) {
 }
 
 function filterBtn(listValue) {
-	const data_filter_btn = document.querySelector("[data-filter-btn]")
+	const data_filter_btn = document.querySelector('[data-filter-btn]')
 	const listDom = data_filter_btn.content.cloneNode(true).children[0]
 	// console.log(listDom)
 	listDom.value = listValue
-	const btnImg = listDom.querySelector("img")
-	const span = listDom.querySelector("span")
+	const btnImg = listDom.querySelector('img')
+	const span = listDom.querySelector('span')
 	span.textContent = listValue
 	listDom.prepend(span)
 	// getting the right value (image/span/button) clicked
-	listDom.addEventListener("click", (e) => {
+	listDom.addEventListener('click', (e) => {
 		let itemValue = null
 		e.stopPropagation()
 		itemValue = getBtnValue(e.target)
@@ -95,13 +95,13 @@ function filterBtn(listValue) {
 function getBtnValue(node) {
 	let btnVal = null
 	switch (node.nodeName) {
-		case "SPAN":
+		case 'SPAN':
 			btnVal = node.textContent
 			break
-		case "IMG":
+		case 'IMG':
 			btnVal = node.parentNode.value
 			break
-		case "BUTTON":
+		case 'BUTTON':
 			btnVal = node.value
 			break
 		default:
@@ -112,9 +112,8 @@ function getBtnValue(node) {
 
 // remove an element from filter list
 function removeItem(itemValue, itemsArray) {
-	console.log(itemValue, itemsArray)
 	const itemIndx = itemsArray.indexOf(itemValue)
-	const btns_node_array = document.querySelectorAll(".badge")
+	const btns_node_array = document.querySelectorAll('.badge')
 	btns_node_array[itemIndx].remove()
 	// btn_node.remove()
 	//console.log(btn_node)
@@ -122,13 +121,15 @@ function removeItem(itemValue, itemsArray) {
 	if (itemIndx > 0) {
 		itemsArray.splice(itemIndx, 1)
 	}
+	filterDomByTags(itemValue, 'card-ingredients')
+	console.log(itemValue, itemsArray)
 }
 
 // checks if value is repetaed and then adds it to an array
 // accepts a value(String) and an Array
 // it also adds the button to the DOM if necessary
 function checkIncludes(value, arrayType) {
-	const filters_container = document.querySelector("[data-filters]")
+	const filters_container = document.querySelector('[data-filters]')
 	if (!arrayType.includes(value)) {
 		arrayType.push(value)
 		if (arrayType === ingredientsArrayFilter) {
@@ -139,20 +140,19 @@ function checkIncludes(value, arrayType) {
 
 // will return a ul element filled with items with a class "list-group-item"
 export function domLists(elementsArray) {
-	const ul = document.createElement("ul")
-	ul.classList.add("list-group")
-	ul.classList.add("list-group-horizontal")
-	ul.classList.add("flex-wrap")
-	ul.classList.add("justify-content-around")
+	const ul = document.createElement('ul')
+	ul.classList.add('list-group')
+	ul.classList.add('list-group-horizontal')
+	ul.classList.add('flex-wrap')
+	ul.classList.add('justify-content-around')
 
 	elementsArray.forEach((element) => {
-		const li = document.createElement("li")
-		li.classList.add("list-group-item")
-		li.addEventListener("click", (e) => {
+		const li = document.createElement('li')
+		li.classList.add('list-group-item')
+		li.addEventListener('click', (e) => {
 			e.stopPropagation()
 			handleListClick(e.target.textContent)
-			filterDom(e.target.textContent, "card-ingredients")
-			console.log("running the filter click")
+			filterDomByTags(e.target.textContent, 'card-ingredients')
 		})
 		li.textContent = element
 		ul.append(li)
--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-import domCard from "./factories/CardFn.js"
+import domCard from './factories/CardFn.js'
 import {
 	ingredientsArray,
+	ingredientsArrayFilter,
 	ustensilsArray,
 	appliance,
 	domLists,
-} from "./factories/CardFn.js"
+} from './factories/CardFn.js'
 
-import { recipes } from "../data/recipes.js"
+import { recipes } from '../data/recipes.js'
 
 let cards = []
 let largeBtn = null
+// filteredCards
+let filteredCards = []
 
-const cards_container = document.querySelector("[data-cards-container]")
+const cards_container = document.querySelector('[data-cards-container]')
 const ingredients_btn = document.querySelector('[data-btn="ingredients"]')
 const appliance_btn = document.querySelector('[data-btn="appliance"]')
 const ustensils_btn = document.querySelector('[data-btn="ustensils"]')
-const rechGeneral = document.querySelector("#Rechercher")
+const rechGeneral = document.querySelector('#Rechercher')
 
 cards = recipes.map((card) => {
 	const cardReady = domCard(card)
@@ -27,26 +30,31 @@ cards = recipes.map((card) => {
 	}
 })
 
-rechGeneral.addEventListener("input", (e) => {
+// listens to input and filters
+rechGeneral.addEventListener('input', (e) => {
 	const value = e.target.value.toLowerCase()
 	if (value.length >= 3) {
-		filterDom(value, "card-name")
+		filterDom(value)
 	}
-})
-
-document.querySelector("body").addEventListener("click", (e) => {
-	closeBigBtn()
-})
-
-rechGeneral.addEventListener("keydown", (e) => {
-	const value = e.target.value.toLowerCase()
-	if (e.key == "Backspace") {
+	if (value === '') {
 		filterDom(value)
 	}
 })
 
+// listens to "delete button" (backspace)
+rechGeneral.addEventListener('keydown', (e) => {
+	const value = e.target.value.toLowerCase()
+	if (e.key == 'Backspace') {
+		filterDom(value)
+	}
+})
+
+document.querySelector('body').addEventListener('click', (e) => {
+	closeBigBtn()
+})
+
 // ingredients click
-ingredients_btn.addEventListener("click", (e) => {
+ingredients_btn.addEventListener('click', (e) => {
 	closeBigBtn()
 	e.stopPropagation()
 	largeBtn = e.target.parentElement.parentElement.parentElement
@@ -54,14 +62,14 @@ ingredients_btn.addEventListener("click", (e) => {
 	largeBtn.append(domLists(ingredientsArray))
 })
 
-appliance_btn.addEventListener("click", (e) => {
+appliance_btn.addEventListener('click', (e) => {
 	closeBigBtn()
 	e.stopPropagation()
 	largeBtn = e.target.parentElement.parentElement.parentElement
 	openBigBtn()
 })
 
-ustensils_btn.addEventListener("click", (e) => {
+ustensils_btn.addEventListener('click', (e) => {
 	closeBigBtn()
 	e.stopPropagation()
 	largeBtn = e.target.parentElement.parentElement.parentElement
@@ -70,38 +78,75 @@ ustensils_btn.addEventListener("click", (e) => {
 
 function closeBigBtn() {
 	if (largeBtn) {
-		largeBtn.style.width = "170px"
-		largeBtn.style.maxHeight = "69px"
-		largeBtn.style.overflow = "inherit"
+		largeBtn.style.width = '170px'
+		largeBtn.style.maxHeight = '69px'
+		largeBtn.style.overflow = 'inherit'
 		largeBtn = null
 	}
 }
 
 function openBigBtn() {
-	largeBtn.style.width = "667px"
-	largeBtn.style.maxHeight = "397px"
-	largeBtn.style.overflow = "auto"
+	largeBtn.style.width = '667px'
+	largeBtn.style.maxHeight = '397px'
+	largeBtn.style.overflow = 'auto'
 }
 
 // filters the cards based on he big research
-export function filterDom(value, nameElem) {
-	value = value.toLowerCase()
+export function filterDom(value) {
 	cards.forEach((card) => {
-		if (nameElem === "card-name") {
-			const isVisible = card.name.toLowerCase().includes(value)
-			card.element.classList.toggle("d-none", !isVisible)
-		}
-		if (nameElem === "card-ingredients") {
-			card.ingredients.forEach((ingObj) => {
-				const isVisible = ingObj.ingredient.toLowerCase().includes(value)
-				console.log(value)
-				card.element.classList.toggle("d-none", !isVisible)
-			})
-		}
+		const { name } = card
+		const isVisible = name.toLowerCase().includes(value)
+		card.element.classList.toggle('d-none', !isVisible)
 	})
 }
 
+// HEREEEE
 // filter by ingredient
-function filterDomIng() {
-	console.log("run the ingredients fn ... ")
+export function filterDomByTags(value, nameElem) {
+	// cards:  array with all recepies (objects{name: string / ingredients: [Objects] / element: Node})
+	// ingredientsArrayFilter : array with all filters
+	// filteredCards: array with cards that contain certain words
+	cards_container.textContent = ''
+
+	// filter by ingredient tag
+	if (nameElem === 'card-ingredients') {
+		// check if there is a filter tag already
+		if (filteredCards.length === 0) {
+			// clear the dom container
+			// search though ALL cards with no filters
+			searchByTagName(value, cards)
+			console.log(filteredCards)
+			filteredCards.forEach((filteredCrd) => {
+				cards_container.append(filteredCrd.element)
+			})
+		} else {
+			//searchByTagName(value, filteredCards)
+			let tempArr = []
+			filteredCards.forEach((filteredCrd) => {
+				const { ingredients } = filteredCrd
+				ingredients.forEach((ing) => {
+					if (ing.ingredient === value) {
+						tempArr.push(filteredCrd)
+					}
+				})
+				filteredCards = tempArr
+			})
+			filteredCards.forEach((filteredCrd) => {
+				cards_container.append(filteredCrd.element)
+			})
+			console.log(filteredCards)
+		}
+	}
+}
+
+function searchByTagName(value, arrayElement) {
+	arrayElement.forEach((card) => {
+		const { ingredients } = card
+		// for each ingredient of each card obj
+		ingredients.forEach((ing) => {
+			if (ing.ingredient === value) {
+				filteredCards.push(card)
+			}
+		})
+	})
 }
